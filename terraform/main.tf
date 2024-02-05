dde95d42ae6692a553d14bb82713c7b81e5c7c45
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-06 00:13:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,14 +7,14 @@ provider "google" {
 resource "google_dataproc_cluster" "my_dataproc_cluster" {
   name           = "my_dataproc_cluster"
   project        = "root-rock-413418"
-  region         = "us-central1"
+  region         = "us-west4"
   cluster_config {
     master_config {
       num_instances = 1
       machine_type  = "n1-standard-4"
     }
     worker_config {
-      num_instances = 2
+      num_instances = 1
       machine_type  = "n1-standard-4"
     }
   }
