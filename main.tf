08a6b49f0eca5d0eb23d52c880824c9293e27380
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-05 19:52:11 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+# File: terraform/main.tf
+provider "google" {
+  project = "root-rock-41341"
+  region  = "us-central1"
+}
+
+resource "google_dataproc_cluster" "my_dataproc_cluster" {
+  name           = "my_dataproc_cluster"
+  project        = "root-rock-413418"
+  region         = "us-central1"
+  cluster_config {
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-4"
+    }
+    worker_config {
+      num_instances = 2
+      machine_type  = "n1-standard-4"
+    }
+  }
+}
