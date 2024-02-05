9367930142f8e4b54750497b72fb068aa939e6b7
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-05 13:15:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,22 @@
+# File: terraform/main.tf
+provider "google" {
+  project = "your-project-id"
+  region  = "us-central1"
+}
+
+resource "google_dataproc_cluster" "example_cluster" {
+  name           = "example-cluster"
+  project        = "your-project-id"
+  region         = "us-central1"
+  cluster_config {
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-4"
+    }
+    worker_config {
+      num_instances = 2
+      machine_type  = "n1-standard-4"
+    }
+  }
+}
 
