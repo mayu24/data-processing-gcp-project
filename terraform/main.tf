d6ada8af81225e9a3819e661d70149abea4c57bc
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-06 00:50:30 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 resource "google_dataproc_cluster" "my_dataproc_cluster" {
   name           = "my_dataproc_cluster"
-  project        = "root-rock-413418"
+  project        = "root-rock-41341"
   region         = "us-central1"
   cluster_config {
     master_config {
