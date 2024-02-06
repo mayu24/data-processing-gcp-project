a4387956604cdfdd9fa0f7c507fdcfd61b52435d
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-06 01:46:44 +0100

    Update process_data.py

--- cloud_functions/process_data.py
+++ cloud_functions/process_data.py
@@ -9,7 +9,7 @@ def process_data(event, context):
     print(f"Processing file: {file_name}")
 
     # Read the file content from Cloud Storage
-    file_blob = storage_client.bucket(bucket_name).get_blob(file_name)
+    file_blob = storage_client.bucket(my_bucket_dataproc).get_blob(file_name)
     file_content = file_blob.download_as_string().decode('utf-8')
 
     # Preprocess the data
@@ -17,7 +17,7 @@ def process_data(event, context):
 
     # Perform further processing or store the preprocessed data
     # For example, you can write the preprocessed data to another Cloud Storage bucket
-    preprocessed_blob = storage_client.bucket(preprocessed_bucket_name).blob(file_name)
+    preprocessed_blob = storage_client.bucket(my_bucket_dataproc_preprocess).blob(file_name)
     preprocessed_blob.upload_from_string(preprocessed_content, content_type='text/plain')
 
     print("Data preprocessing completed.")
