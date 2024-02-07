01853d4616638f725efd3091fa2a2e37bc0ad5ed
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-07 20:43:55 +0100

    Update data_processing_dag.py

--- composer/data_processing_dag.py
+++ composer/data_processing_dag.py
@@ -1,12 +1,11 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+from airflow.providers.google.operators.dataproc import DataprocCreateClusterOperator, DataprocSubmitJobOperator, DataprocDeleteClusterOperator
 from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2024, 2, 7),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -14,29 +13,28 @@ default_args = {
 }
 
 dag = DAG(
-    'data_processing_dag',
+    'dataproc_processing_dag',
     default_args=default_args,
-    description='Data Processing DAG',
-    schedule_interval='@daily',
+    description='Dataproc Processing DAG',
+    schedule_interval=None,  # Set to None or a specific schedule interval
 )
 
-create_table_task = BigQueryCreateEmptyTableOperator(
-    task_id='create_table',
-    dataset_id='your_dataset',
-    table_id='processed_data',
-    gcp_conn_id='google_cloud_default',
+create_cluster_task = DataprocCreateClusterOperator(
+    task_id='create_dataproc_cluster',
+    project_id='root-rock-413418',
+    region='us-central1',
+    cluster_name='my_dataproc_cluster',
+    num_workers=2,
     dag=dag
 )
 
 submit_dataproc_job_task = DataprocSubmitJobOperator(
     task_id='submit_dataproc_job',
     job_name='dataproc_job',
-    gcp_conn_id='google_cloud_default',
-    project_id='root-rock-413418',
     cluster_name='my_dataproc_cluster',
-    region='us-central1',
+    region='your_region',
     job={
-        'reference': {'projectId': 'root-rock-413418'},
+        'reference': {'projectId': 'your_project_id'},
         'placement': {'clusterName': 'my_dataproc_cluster'},
         'pysparkJob': {
             'mainPythonFileUri': 'gs://my_bucket_dataproc/my_script.py',
@@ -45,4 +43,13 @@ submit_dataproc_job_task = DataprocSubmitJobOperator(
     dag=dag
 )
 
-create_table_task >> submit_dataproc_job_task
+delete_cluster_task = DataprocDeleteClusterOperator(
+    task_id='delete_dataproc_cluster',
+    project_id='root-rock-413418',
+    region='us-central1',
+    cluster_name='my_dataproc_cluster',
+    trigger_rule='all_success',  # This task runs only if the previous tasks are successful
+    dag=dag
+)
+
+create_cluster_task >> submit_dataproc_job_task >> delete_cluster_task
