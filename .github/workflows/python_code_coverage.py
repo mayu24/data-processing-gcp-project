5c19bf4342136f2dc8bc834e29d38385b73c5daa
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-15 17:57:04 +0100

    Create python_code_coverage.py

---
+++ .github/workflows/python_code_coverage.py
@@ -0,0 +1,16 @@
+            - name: Python Coverage
+  # You may pin to the exact commit or the version.
+  # uses: orgoro/coverage@4e3bc9302f715595336a40f7dd4a3b15c687bdb4
+  uses: orgoro/coverage@v3.1
+  with:
+    # local path to a covergae xml file like the output of pytest --cov
+    coverageFile: 
+    # github token
+    token: 
+    # the coverage threshold for average over all files [0,1]
+    thresholdAll: # optional, default is 0.0
+    # the coverage threshold for average over new files [0,1]
+    thresholdNew: # optional, default is 0.0
+    # the coverage threshold for average over new files [0,1]
+    thresholdModified: # optional, default is 0.0
+          
