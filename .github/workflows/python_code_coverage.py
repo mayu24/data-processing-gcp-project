a77d03f6d65d2ac8c4d783b67d3f85526049aa33
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-15 18:46:57 +0100

    Update python_code_coverage.py

--- .github/workflows/python_code_coverage.py
+++ .github/workflows/python_code_coverage.py
@@ -1,35 +1,30 @@
-name: Python Coverage
+name: Python Testing and Coverage
 
 on:
   push:
     branches:
       - master
+  pull_request:
+    branches:
+      - master
 
 jobs:
-  my_job:
-    runs-on: ubuntu-18.04
+  test:
+    runs-on: ubuntu-latest
 
     steps:
-      - name: Python Coverage
-        uses: orgoro/coverage@v3.1
-        with:
-          coverageFile: ${{ secrets.COVERAGE_FILE }}
-          token: ${{ secrets.GITHUB_TOKEN }}
-          thresholdAll: 0.0
-          thresholdNew: 0.0
-          thresholdModified: 0.0
+      - name: Checkout code
+        uses: actions/checkout@v2
 
-      - name: Upload coverage report
-        uses: actions/upload-artifact@v2
+      - name: Set up Python
+        uses: actions/setup-python@v2
         with:
-          name: coverage-report
-          path: ${{ secrets.COVERAGE_FILE }}
+          python-version: 3.9
 
-      - name: Comment on pull request with coverage report
-        uses: unsplash/comment-on-pr@v1
-        with:
-          msg: |
-            Coverage Report:
-            ![Coverage Report](https://github.com/${{ github.repository }}/raw/main/${{ secrets.COVERAGE_FILE }})
-        env:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+      - name: Install dependencies
+        run: |
+          pip install pytest pytest-cov
+
+      - name: Run tests with coverage
+        run: |
+          pytest --cov=data-processing-gcp-project
