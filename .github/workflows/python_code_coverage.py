3045ef66548c38074c1b458fcb1ca58bce976c84
Mayur Mahajan <mayuryesh@gmail.com>
2024-02-15 18:22:48 +0100

    Update python_code_coverage.py

--- .github/workflows/python_code_coverage.py
+++ .github/workflows/python_code_coverage.py
@@ -1,28 +1,28 @@
----
 name: Deploy
+
 on:
   push:
     branches:
-    - main
+      - main
 
 jobs:
   my_job:
-    name: deploy to Cloud Strorage 
     runs-on: ubuntu-18.04
 
-- name: Python Coverage
-  # You may pin to the exact commit or the version.
-  # uses: orgoro/coverage@4e3bc9302f715595336a40f7dd4a3b15c687bdb4
-  uses: orgoro/coverage@v3.1
-  with:
-    # local path to a covergae xml file like the output of pytest --cov
-    coverageFile: 
-    # github token
-    token: 
-    # the coverage threshold for average over all files [0,1]
-    thresholdAll: # optional, default is 0.0
-    # the coverage threshold for average over new files [0,1]
-    thresholdNew: # optional, default is 0.0
-    # the coverage threshold for average over new files [0,1]
-    thresholdModified: # optional, default is 0.0
+    steps:
+      - name: Python Coverage
+        # You may pin to the exact commit or the version.
+        # uses: orgoro/coverage@4e3bc9302f715595336a40f7dd4a3b15c687bdb4
+        uses: orgoro/coverage@v3.1
+        with:
+          # Local path to a coverage xml file like the output of pytest --cov
+          coverageFile: ${{ secrets.COVERAGE_FILE }}
+          # GitHub token
+          token: ${{ secrets.GITHUB_TOKEN }}
+          # The coverage threshold for average over all files [0,1]
+          thresholdAll: # optional, default is 0.0
+          # The coverage threshold for average over new files [0,1]
+          thresholdNew: # optional, default is 0.0
+          # The coverage threshold for average over modified files [0,1]
+          thresholdModified: # optional, default is 0.0
           
